f8fb8e8632be3da47db1e42dff60d2730d556c74
HenryCodeT <120686@unsaac.edu.pe>
2022-02-14 12:11:13 -0500

    initial

---
+++ PrimerPrograma.java
@@ -0,0 +1,7 @@
+public class PrimerPrograma {
+    public static void main(String[] args) {
+     System.out.println("Mi nombre es Coding Dojo");   
+     System.out.println("Tengo 100 anios de edad");   
+     System.out.println("Mi ciudad natal es Burbank, CA");   
+    }
+}
